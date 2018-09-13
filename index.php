536400e2a3a73da04d3f089c8975adfde191df2f
triinoxys <contact@triinoxys.fr>
2018-09-14 01:48:51 +0200

    :construction: Basic "viewers game" tool

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require "secrets.php";
+
+try { $bdd = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass); }
+catch (Exception $e) { die('Erreur de connexion à la BDD: ' . $e->getMessage()); }
+
+function get_viewers($bdd) {
+    foreach  ($bdd->query('SELECT id, twitch_name, ingame_name FROM game_viewers') as $row) {
+        print $row['id'] . "&emsp;";
+        print  $row['twitch_name'] . "&emsp;";
+        print $row['ingame_name'] . "<br>";
+  }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Souquatachi</title>
+</head>
+
+<body>
+  <h1>Sukatshi - Game Viewers</h1>
+  <?php
+    get_viewers($bdd);
+  ?>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,21 @@
+<?php
+require "secrets.php";
+
+try { $bdd = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass); }
+catch (Exception $e) { die("Connexion à la BDD impossible. @triinoxys ALED"); }
+
+if (!isset($_GET['twitch_name']))
+  die("Une erreur est survenue. @triinoxys ALED");
+
+if ($resp = $bdd->query("SELECT id FROM `game_viewers` where twitch_name = '" . $_GET['twitch_name'] . "'")->fetch())
+  die("@" . $_GET['twitch_name'] . " tu es déjà enregistré (#" . $resp['id'] . ").");
+
+if (isset($_GET['ingame_name']))
+  $bdd->query("INSERT INTO `game_viewers`(`twitch_name`, `ingame_name`) VALUES ('" . $_GET['twitch_name'] . "', '" . $_GET['ingame_name'] . "')");
+else
+  $bdd->query("INSERT INTO `game_viewers`(`twitch_name`) VALUES ('" . $_GET['twitch_name'] . "')");
+
+echo "@", $_GET['twitch_name'], " enregistré (#", $bdd->query("SELECT id FROM `game_viewers` where twitch_name = '" . $_GET['twitch_name'] . "'")->fetch()['id'], ").";
+
+?>
+
